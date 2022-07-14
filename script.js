8252c1efe4cffd474323c8dd86f9ac322329d7c7
Vitor <vitorcarvalho.dev@gmail.com>
2022-07-13 22:47:18 -0300

    Atualizando sistema de agendamento

--- script.js
+++ script.js
@@ -1,11 +1,51 @@
-const evento = document.getElementById('send')
-const enviarFormulario =() => {
+/* Dados Formulário */ 
+
+const evento = document.getElementById('send');
+function forms() {
         let nome = document.getElementById('nome').value;
-        let tel = document.getElementById('tel').value; 
-        let service = document.getElementById('service').value;
-        let numero= 5571982246892;
+        let tel = document.getElementById('tel').value;
+        let service = document.getElementById('service').value; 
+        let msg = document.getElementById('msg');
+        let numero = 5571986894235; 
+        let valor1 = 'R$ 20,00';
+        let valor2 = 'R$ 25,00';
+        let valor3 = 'R$ 30,00';
         
+
+        if (service === 'Mão') {
+                 
+                var win = window.open(`https://wa.me/${numero}?text=%20%20%20*LarryFashion*%20%20%20 %0A%0A------------------------ 
+        %0A%0ACliente:%20${nome} %0A%0ATelefone:%20${tel}%0A%0A------------------------ 
+        %0A%0AServiço:%20${service}%0A%0A------------------------ %0A%0A*Valor*: ${valor1}`,'_blank');
+        }  
+
+        if (service === 'Pé') {
+                 
+                var win = window.open(`https://wa.me/${numero}?text=%20%20%20*LarryFashion*%20%20%20 %0A%0A------------------------ 
+        %0A%0ACliente:%20${nome} %0A%0ATelefone:%20${tel}%0A%0A------------------------ 
+        %0A%0AServiço:%20${service}%0A%0A------------------------ %0A%0A*Valor*: ${valor2}`,'_blank');
+        }  
+
+        if (service === 'Mão e Pé') {
+                
+                var win = window.open(`https://wa.me/${numero}?text=%20%20%20*LarryFashion*%20%20%20 %0A%0A------------------------ 
+        %0A%0ACliente:%20${nome} %0A%0ATelefone:%20${tel}%0A%0A------------------------ 
+        %0A%0AServiço:%20${service}%0A%0A------------------------ %0A%0A*Valor*: ${valor3}`,'_blank');
+        } 
+
+        if (service === 'Selecione') {
+                msg.innerHTML = `Selecione um serviço !`
+        }
+                
+
         
-var win= window.open(`https://wa.me/${numero}?text=%20%20%20*LarryFashion*%20%20%20 %0A%0A------------------------ %0A%0ACliente:%20${nome} %0A%0ATelefone:%20${tel}%0A%0A------------------------ %0A%0AServiço:%20${service}`,'_blank');       
 }
-evento.addEventListener('click', enviarFormulario)
+
+ 
+
+evento.addEventListener('click', forms);
+
+
+
+        
+
