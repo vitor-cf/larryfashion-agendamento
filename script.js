a64441f229568a1d4290149bff179c58524910bb
Vitor <vitorcarvalho.dev@gmail.com>
2022-07-14 00:15:22 -0300

    Atualizando sistema de agendamento

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ function forms() {
         let nome = document.getElementById('nome').value;
         let tel = document.getElementById('tel').value;
         let service = document.getElementById('service').value; 
-        let msg = document.getElementById('msg');
+        let func = document.getElementById('func').value;
+        let msg = document.getElementById('msg'); 
+        
         let numero = 5571991182502; 
         let valor1 = 'R$ 20,00';
         let valor2 = 'R$ 25,00';
@@ -15,7 +17,7 @@ function forms() {
         if (service === 'Mão') { 
                 
                 var win = window.open(`https://wa.me/${numero}?text=%20%20%20*LarryFashion*%20%20%20 %0A%0A------------------------ 
-        %0A%0ACliente:%20${nome} %0A%0ATelefone:%20${tel}%0A%0A------------------------ 
+        %0A%0ACliente:%20${nome} %0A%0ATelefone:%20${tel}%0A%0A %0A%0ATelefone:%20${func}%0A%0A------------------------ 
         %0A%0AServiço:%20${service}%0A%0A------------------------ %0A%0A*Valor*: ${valor1}`,'_blank');
         }  
                  
@@ -36,14 +38,21 @@ function forms() {
         } 
 
         if (service === 'Selecione') {
-                msg.innerHTML = `Selecione um serviço !`
+                document.getElementById('d-msg').style.display ='block';
+                msg.innerHTML = `Selecione um serviço !`; 
+                
         }
                 
 
         
+}  
+
+const feche = document.getElementById('d-msg');
+function buttonFechar() {
+        document.getElementById('d-msg').style.display = 'none';
 }
 
- 
+feche.addEventListener('click', buttonFechar) 
 
 evento.addEventListener('click', forms);
 
